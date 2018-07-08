267d6e0eecefa116f8f8e7bdb57e3f86411f7939
drproduck <duckhiem95@yahoo.com>
2018-07-07 23:46:46 -0700

    refractor

--- SPECTRAL_CLUSTERING/coclustering_test/20news_origin/make_matrix.m
+++
@@ -1,5 +0,0 @@
-load('20NewsHome.mat')
-f=fopen('vocabulary.txt');
-vocab=textscan(f,'%s');
-vocab=vocab{1};
-save('20newsorigin','fea','gnd','vocab');
---
+++ main/experiment/untitled.m
@@ -0,0 +1,9 @@
+
+
+opts.sigma = 5;
+opts = change(opts);
+opts.sigma
+
+function [opts] = change(opts)
+opts.sigma = 7;
+end
